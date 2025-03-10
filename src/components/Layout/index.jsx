67bda2b8cf29ff11a1f33d26697d4d4b547f910e
Lukasz Nogaj <lukanogaj@hotmail.co.uk  git config --global user.name lukanogaj>
2025-03-10 10:45:32 +0000

    create-react-app

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,17 @@
+import styles from "./index.module.scss";
+import PropTypes from "prop-types";
+// import { useState } from "react";
+// import useLocalStorage from "use-local-storage";
+
+const Layout = ({ children }) => {
+	return (
+		<div className={styles.layoutContainer}>
+			<div className={styles.layout}>{children}</div>
+		</div>
+	);
+};
+
+Layout.propTypes = {
+	children: PropTypes.element,
+};
+export default Layout;
