f1fd480041512f7920be647ed585469854197199
Lukasz Nogaj <lukanogaj@hotmail.co.uk  git config --global user.name lukanogaj>
2025-03-12 08:30:25 +0000

    navStart-map

--- src/components/Navabar/index.jsx
+++ src/components/Navabar/index.jsx
@@ -1,15 +1,27 @@
 import styles from "./index.module.scss";
+import data from "../data";
 
 const Navbar = () => {
 	return (
 		<div className={styles.navbar}>
-			<div className={styles.navStart}>
+			{data.navbarStar.map((navitem) => (
+				<div
+					className={styles.navStart}
+					key={navitem.id}>
+					<div className={styles.copyEmail}>
+						<div className={styles.copyEmail}> {navitem.email}</div>
+						<div className={styles.copy}> {navitem.copy}</div>
+					</div>
+					<div className={styles.cv}>{navitem.cv}</div>
+				</div>
+			))}
+			{/* <div className={styles.navStart}>
 				<div className={styles.copyEmail}>
 					<div className={styles.copyEmail}>lukan.hotmail.co.uk</div>
 					<div className={styles.copy}>Copy</div>
 				</div>
 				<div className={styles.cv}>CV</div>
-			</div>
+			</div> */}
 			<div className={styles.navEnd}>
 				<div className={styles.links}>Linkedin</div>
 				<div className={styles.links}>/</div>
---
+++ src/components/data/index.jsx
@@ -0,0 +1,22 @@
+const data = {
+	navbarStar: [
+		{
+			id: 1,
+			email: "lukan.hotmail.co.uk",
+			copy: "Copy",
+			cv: "Cv",
+		},
+	],
+	navbarEnd: [
+		{
+			id: 2,
+			linkedin: "Linkedin",
+			slash: "/",
+			dribble: "Dribble",
+			slash: "/",
+			instagram: "Instagram",
+		},
+	],
+};
+
+export default data;
