31d5218f53727822ea56775b23eb012a9bcf4539
Lukasz Nogaj <lukanogaj@hotmail.co.uk  git config --global user.name lukanogaj>
2025-03-18 07:09:19 +0000

    divison-css-classes

--- src/components/Division/index.jsx
+++ src/components/Division/index.jsx
@@ -3,12 +3,12 @@ import styles from "./index.module.scss";
 const Division = () => {
 	return (
 		<div className={styles.divison}>
-			<div class="services_heading">
+			<div class="servicesHead">
 				<h2>
 					Collaborate with brands and agencies <br />
 					to create impactful results.
 				</h2>
-				<div class="services_button">
+				<div class="servicesBtn">
 					<h4>Services</h4>
 				</div>
 				<hr />
@@ -16,68 +16,68 @@ const Division = () => {
 			{/* Divs with the services icons and descriptions  */}
 
 			<div class="services">
-				<div class="services_wrapper">
+				<div class="servicesWrapper">
 					<div class="services_image">
 						<img
 							src="./images/Icons/uiux.svg"
 							alt=""
 						/>
 					</div>
-					<div class="services_head">
+					<div class="servicesHead">
 						<h3>UX & UI</h3>
 					</div>
-					<div class="services_paragraph">
+					<div class="servicesParagraph">
 						<p>
 							Designing interfaces that are intuitive, efficient, and enjoyable
 							to use.
 						</p>
 					</div>
 				</div>
-				<div class="services_wrapper">
+				<div class="servicesWrapper">
 					<div class="services_image">
 						<img
 							src="./images/Icons/app.svg"
 							alt=""
 						/>
 					</div>
-					<div class="services_head">
+					<div class="servicesHead">
 						<h3>Web & Mobile App</h3>
 					</div>
-					<div class="services_paragraph">
+					<div class="servicesParagraph">
 						<p>
 							Transforming Ideas into exceptional web and mobile app
 							experiences.
 						</p>
 					</div>
 				</div>
-				<div class="services_wrapper">
-					<div class="services_image">
+				<div class="servicesWrapper">
+					<div class="servicesImage">
 						<img
 							src="./images/Icons/design.svg"
 							alt=""
 						/>
 					</div>
-					<div class="services_head">
+					<div class="servicesHead">
 						<h3>Design & Creative</h3>
 					</div>
-					<div class="services_paragraph">
+					<div class="servicesParagraph">
 						<p>
 							Crafting visually stunning designs that connect with your
 							audience.
 						</p>
 					</div>
 				</div>
-				<div class="services_wrapper">
-					<div class="services_image">
+				<div class="servicesWrapper">
+					<div class="servicesImage">
 						<img
 							src="./images/Icons/development.svg"
 							alt=""
 						/>
 					</div>
-					<div class="services_head">
+					<div class="servicesHead">
 						<h3>Development</h3>
 					</div>
-					<div class="services_paragraph">
+					<div class="servicesParagraph">
 						<p>
 							Bringing your vision to life with the latest technology and design
 							trends.
